1043beae937c925ebdf86b8ec098109964eae2ed
demiddima <demid.dima.1996@gmail.com>
2024-01-22 11:32:13 +1000

    add Lesson3Task1

---
+++ Lesson3Task1/Controllers/ProductsController.cs
@@ -0,0 +1,15 @@
+﻿using Lesson3Task1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lesson3Task1.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            ProductRepo repo = new ProductRepo();
+       
+            return View(repo);
+        }
+    }
+}
---
+++ Lesson3Task1/Models/ProductRepo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Lesson3Task1.Models
+{
+    public class ProductRepo
+    {
+        public List<Product> collection { get; set; }
+
+        public ProductRepo()
+        {
+            collection = new List<Product>() 
+            { 
+                new Product { Id = 1, Name = "Name 1", Price = 10},
+                new Product { Id = 2, Name = "Name 2", Price = 20},
+                new Product { Id = 3, Name = "Name 3", Price = 30},
+            };
+        }
+    }
+}

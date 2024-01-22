22e200a9964bb9ad848976235d2c7329cc93a39c
demiddima <demid.dima.1996@gmail.com>
2024-01-22 12:23:40 +1000

    Add Lessson3Task3 Calculator

---
+++ Calculator/Controllers/CalcController.cs
@@ -0,0 +1,47 @@
+﻿using Calculator.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Calculator.Controllers
+{
+    public class CalcController : Controller
+    {
+        Data data = new Data();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            data.Num1 = int.Parse(Request.Form["num1"]);
+            data.Num2 = int.Parse(Request.Form["num2"]);
+            data.Operation = char.Parse(Request.Form["operation"]);
+
+            if (data.Operation.ToString() == "+")
+            {
+                data.Result = data.Num1 + data.Num2;
+            }
+            else if(data.Operation.ToString() == "-")
+            {
+                data.Result = data.Num1 - data.Num2;
+            }
+            else if (data.Operation.ToString() == "/")
+            {
+                data.Result = data.Num1 / data.Num2;
+            }
+            else
+            {
+                data.Result = data.Num1 * data.Num2;
+            }
+            return View("Result", data);
+        }
+
+
+        public IActionResult Result()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Calculator/Models/Data.cs
@@ -0,0 +1,10 @@
+﻿namespace Calculator.Models
+{
+    public class Data
+    {
+        public int Num1 { get; set; }
+        public int Num2 { get; set; }
+        public char Operation { get; set; }
+        public int Result { get; set; }
+    }
+}

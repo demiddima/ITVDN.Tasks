8e5414ab5f568c965a83923a395516a67658f5e1
demiddima <demid.dima.1996@gmail.com>
2024-01-22 15:22:58 +1000

    Add Lesson5Task12

---
+++ BootStrap/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BootStrap.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Main()
+        {
+            return View();
+        }
+
+        public IActionResult Page1()
+        {
+            return View();
+        }
+
+
+        public IActionResult Page2()
+        {
+            return View();
+        }
+
+    }
+}

6d61007eb6c70eec7b3a9a73ac07ede7b64ec057
demiddima <demid.dima.1996@gmail.com>
2024-01-24 08:56:31 +1000

    Add Lesson10Task1

---
+++ Lesson10Task1/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Lesson10Task1.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lesson10Task1.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Log]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Lesson10Task1/Infrastructure/LogAttribute.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.IO;
+
+namespace Lesson10Task1.Infrastructure
+{
+    public class LogAttribute: ActionFilterAttribute
+    {
+        private string dateTimeNow;
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            dateTimeNow = DateTime.Now.ToString("HH-mm-ss-fff");
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            string message = $"Метод {context.ActionDescriptor.DisplayName} начал выполнятся {dateTimeNow}, а закончился свою работу {DateTime.Now.ToString("HH-mm-ss-fff")}";
+            StreamWriter streamWriter = new StreamWriter("App_Data/log.txt", true);
+            streamWriter.WriteLine(message);
+            streamWriter.Close();
+        }
+    }
+}

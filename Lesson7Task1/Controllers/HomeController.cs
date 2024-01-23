e527a34406c889d0d0b08cd47d6e50f067a0feb6
demiddima <demid.dima.1996@gmail.com>
2024-01-23 10:49:30 +1000

    Add Lesson7Task1

---
+++ Lesson7Task1/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Lesson7Task1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Lesson7Task1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(ProductBindingModel model)
+        {
+            Debug.WriteLine($"Название товара: {model.NameProduct}") ;
+            Debug.WriteLine($"Описание товара: {model.DescriptionProduct}");
+            Debug.WriteLine($"Название товара: {model.Count}");
+
+            return View();
+        }
+    }
+}
---
+++ Lesson7Task1/Models/ProductBindingModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Lesson7Task1.Models
+{
+    public class ProductBindingModel
+    {
+        public string  NameProduct { get; set; }
+        public string DescriptionProduct { get; set; }
+        public int Count { get; set; }
+
+    }
+}

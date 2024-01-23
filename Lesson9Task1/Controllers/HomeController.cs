1144b96510526fb7128a37d81efaf7290a71ed72
demiddima <demid.dima.1996@gmail.com>
2024-01-24 08:31:31 +1000

    Add Lesson9Task1

---
+++ Lesson9Task1/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Lesson9Task1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new List<string> { "red", "green", "blue" };
+
+            return View(model);
+        }
+    }
+}
---
+++ Lesson9Task1/Infrastructure/TagHelpers/DivBorderTagHelper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+
+namespace Lesson9Task1.Infrastructure.TagHelpers
+{
+    public class DivBorderTagHelper : TagHelper
+    {
+        public string MyBorderColor { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (string.Equals(context.TagName, "div", StringComparison.OrdinalIgnoreCase))
+            {
+                string borderColor = MyBorderColor ?? "black";
+
+                // Добавление класса вместо стиля
+                output.Attributes.SetAttribute("class", $"my-bordered-div my-border-{borderColor}");
+            }
+        }
+    }
+}

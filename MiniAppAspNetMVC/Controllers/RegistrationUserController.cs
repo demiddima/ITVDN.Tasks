82f5f1a63fdf09988328d87f2a216ebdb8b0f7fa
demiddima <demid.dima.1996@gmail.com>
2024-01-24 10:37:28 +1000

    instal MiniAppAspNetMVC

---
+++ MiniAppAspNetMVC/Controllers/RegistrationUserController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MiniAppAspNetMVC.Models;
+
+namespace MiniAppAspNetMVC.Controllers
+{
+    public class RegistrationUserController : Controller
+    {
+        [HttpGet]
+        public IActionResult Registration()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Registration(UserRegistration user)
+        {
+            if(user != null && user.isAgree != false)
+            {
+                //add db
+                return View("Accept", user);
+            }
+            else
+            {
+                return NotFound();
+            }
+           
+        }
+
+        public IActionResult Accept()
+        {
+            return View();
+        }
+    }
+}
---
+++ MiniAppAspNetMVC/Models/UserRegistration.cs
@@ -0,0 +1,13 @@
+﻿namespace MiniAppAspNetMVC.Models
+{
+    public class UserRegistration
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public int Age { get; set; }
+        public bool isAgree { get; set; }
+    }
+}
